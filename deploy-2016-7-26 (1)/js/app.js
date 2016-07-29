04047f245593f994c1c37107f4f857f6cb869446
dmaish <mainadaniel81@gmail.com>
2016-07-29 12:51:19 +0300

    commit

--- deploy-2016-7-26 (1)/js/app.js
+++ deploy-2016-7-26 (1)/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+$(document).ready(function(){
+    
+    $("h3").hover(function(){
+        $("h3").css("text-decoration","none");
+        $("h3").css("text-decoration","underline");
+    });
+    
+    $(".par2").hover(function(){
+        $(".par2").css("border-width","thick");
+    });
+    
+});
